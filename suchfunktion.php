4f82956f1311ffe410e2921d12e14b72fa367a2f
Daniel Högel <hoegel.daniel@mail.com>
2016-03-27 16:55:23 +0200

    Code aufräumen

--- suchfunktion.php
+++ suchfunktion.php
@@ -2,12 +2,21 @@
 /* 
 Suchfunktion
 Bearbeiter: Daniel Högel
-Status: 25.03.2016, 18:03 Uhr
+Status: 26.03.2016, 16:00 Uhr
 */
 
 
 require_once('../../../../wp-config.php');
 global $wpdb;
+
+// Erster Buchstabe uppercase, bei Wörtern < 3 Zeichen alles uppercase
+function uppercase($string){
+	if(strlen($string)<3){
+		return strtoupper($string);
+	} else{
+		return ucfirst($string);
+	}
+}
 
 ?>
 
@@ -20,8 +29,9 @@ global $wpdb;
 	<title>Suchfunktion</title>
 </head>
 <body>
-	<main style="max-width: 720px; margin: 0 auto;">
-		<!-- Suchfeld -->
+	<main style="max-width: 720px; margin: 0 auto 10rem;">
+
+<!-- Suchfeld -->
 		<h1>Suche</h1>
 		<form method="GET">
 			<table class="form">
@@ -31,12 +41,6 @@ global $wpdb;
 					</td>
 					<td>
 						<button type='submit' class='search'>Suchen</button>
-					</td>
-				</tr>
-				<tr>
-					<td>
-						
-					
 					</td>
 				</tr>
 			</table>
@@ -49,115 +53,152 @@ global $wpdb;
 				</tr>
 				<tr>
 					<td>
+
 <?php 
+	// Ressort Checkboxen
 	$ressort = $wpdb->get_results("SELECT name FROM Ressort");
 	for ($i = 0; $i < (sizeof($ressort)/2); $i++) {
-		echo "<input type='checkbox' name='ressort' value='".$ressort[$i]->name."' checked> ".$ressort[$i]->name."<br>";
+		echo "<input type='checkbox' name='ressort' value='".$ressort[$i]->name."' checked> ".uppercase($ressort[$i]->name)."<br>";
 	}
 	echo "</td><td>";
 	for ($i; $i < (sizeof($ressort)); $i++) {
-		echo "<input type='checkbox' name='ressort' value='".$ressort[$i]->name."' checked> ".$ressort[$i]->name."<br>";
+		echo "<input type='checkbox' name='ressort' value='".$ressort[$i]->name."' checked> ".uppercase($ressort[$i]->name)."<br>";
 	}
-?>									
+?>	
+
 					</td>
 				</tr>
 			</table>		
 		</form>
 
 <?php 
-
-
-	echo"<br><br>";
-	echo "Suche nach: ";
+	// Suchworte Trennen
+	echo"<br><b>Suche nach:</b> ";
 	$search = explode(" ", trim($_GET['search_text']));
 	foreach ($search as $value) {
-		echo $value.";";
+		echo "<i>$value</i>;";
 	}
-	echo"<br><br>";
 
+	// Suchanfragen für jedes Wort in jeder Spalte vorbereiten
 	for ($i=0; $i < sizeof($search); $i++) { 
-		$search_for_id .= "id LIKE '%".$search[$i]."%'";
-		$search_for_first_name .= "first_name LIKE '%".$search[$i]."%'";
-		$search_for_last_name .= "last_name LIKE '%".$search[$i]."%'";
-		$search_for_birth_date .= "birth_date LIKE '%".$search[$i]."%'";
+		$search_for_id .= "Contact.id LIKE '%".$search[$i]."%'";
+		$search_for_first_name .= "Contact.first_name LIKE '%".$search[$i]."%'";
+		$search_for_last_name .= "Contact.last_name LIKE '%".$search[$i]."%'";
+		$search_for_birth_date .= "Contact.birth_date LIKE '%".$search[$i]."%'";
+		$search_for_ressort_name .= "Ressort.name LIKE '%".$search[$i]."%'";
 		if ($i<(sizeof($search) - 1)) {
 			$search_for_id .= " OR ";
 			$search_for_first_name .= " OR ";
 			$search_for_last_name .= " OR ";
 			$search_for_birth_date .= " OR ";
+			$search_for_ressort_name .= " OR ";
 		}
 
 	}
+
 
 	// Sortieren
 	if (isset($_POST['sort'])){					
 		$order = $_POST['sort'];
 	}
 	else{
-		$order = "id";
+		$order = "Contact.id";
 	}
-	echo "Sortieren nach: ";
-	echo $order;
+	echo "&nbsp;&nbsp;&nbsp; <b>Sortieren nach:</b> <i>$order</i>";
 	echo"<br><br>";
 
-	
+
+	// Datenabankabfrage
 	$results = $wpdb->get_results("
 		SELECT 
-			id, first_name, last_name, birth_date
+			Contact.id, Contact.first_name, Contact.last_name, Contact.birth_date, Ressort.name, Member.active
 		FROM 
 			Contact
+		JOIN 
+			Member ON Contact.id = Member.contact
+		JOIN 
+			Ressort ON Member.ressort = Ressort.id
 		WHERE		
 			$search_for_id OR 
 			$search_for_first_name OR 
 			$search_for_last_name OR 
-			$search_for_birth_date
-		ORDER BY $order
-		");
+			$search_for_birth_date OR
+			$search_for_ressort_name
 
-	//var_dump($results);
-	echo"<br><br>";
-
+		ORDER BY $order,Contact.last_name,Contact.first_name,Ressort.name
+	");
 
 
 
-
+	// Ergebnis der Datenbankabfrage von den Inhalten, die tatsächlich ausgegeben werden trennen
 	$output = $results;
 
-	// Geburtsdatum in Alter umwandeln
+	// Suchergebnisse anpassen
 	for ($i = 0; $i < sizeof($output); $i++) {		
+		
+		// Geburtsdatum in Alter umwandeln
 		$date1 = date_create($output[$i]->birth_date);
 		$date2 = date_create("now");
 		$alter = date_diff($date1, $date2);
 		$output[$i]->birth_date = $alter->format('%y');
-	}
-?>
 
+		// HHC Status
+		if($output[$i]->active == 0){
+			$output[$i]->active = "Aktiv";
+		} else{
+			$output[$i]->active = "Passiv";
+		}
+
+		// Ressort uppercase
+		$output[$i]->name = uppercase($output[$i]->name);
+
+		// Nummerierung hinzufügen
+		//$output[$i] = array('nr' => $i) + $output[$i];
+	}
+
+
+	// Beginn Tabelle für Sucheregbnisse
+	echo "
 		<h2>Suchergebnisse</h2>
-		<form method="POST">
-			<table class='liste' style="width:720px;">
+		<form method='POST'>
+			<table class='liste' style='width:720px;'>
 				<tr>
-					<th>
-						<button type="submit" name="sort" value="id" class="sort <?php if($order == 'id'){echo 'active';} ?>">
-							ID
-						</button>
-					</th>
-					<th>
-						<button type="submit" name="sort" value="first_name" class="sort <?php if($order == 'first_name'){echo 'active';} ?>" >
-							Vorname
-						</button>
-					</th>
-					<th>
-						<button type="submit" name="sort" value="last_name" class="sort <?php if($order == 'last_name'){echo 'active';} ?>" >
-							Nachname
-						</button>
-					</th>
-					<th>
-						<button type="submit" name="sort" value="birth_date" class="sort <?php if($order == 'birth_date'){echo 'active';} ?>" >
-							Alter
-						</button>
-					</th>
-				</tr>
-<?php 
+		";
+
+	// Table Header
+	$t_header = array(
+		array('value' => 'Contact.id', 'name' => 'ID'),
+		array('value' => 'Contact.first_name', 'name' => 'Vorname'),
+		array('value' => 'Contact.last_name', 'name' => 'Nachname'), 
+		array('value' => 'Contact.birth_date', 'name' => 'Alter'), 
+		array('value' => 'Ressort.name', 'name' => 'Ressort'),
+		array('value' => 'Member.active', 'name' => 'Status')
+	);	
+
+	// Print Tabelle Header
+	foreach ($t_header as $value) {
+		
+		// Hinzufügen einer CSS-Klasse zur Identifizierung der Sortierspalte
+		if($order == $value[value]){
+			$active = ' active';
+		}else{
+			$active = "";
+		}
+
+		// Print einzelne Header-Zellen
+		echo "
+			<th>
+				<button type='submit' name='sort' value='".$value[value]."' class='sort$active'>
+					".$value[name]."
+				</button>
+			</th>
+		";
+	}
+
+	echo "</tr>";
+	
+
+	// Print Search Results
 	foreach ($output as $row) {
 		echo "<tr>";
 			foreach ($row as $col) {
