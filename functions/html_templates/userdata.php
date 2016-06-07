2d595bd0ce9d7f4c155fcf3e6a5b2dc2f0b75aec
Alexander Schäfer <alexander.schaefer@uni-duesseldorf.de>
2016-06-07 09:46:45 +0200

    Inputs leicht angepasst
    Auf Serverseite können die Eingabedaten nun etwas besser verarbeitet werden. Alle Inputs, die mehrmals vorkommen können (expandablecontent), werden nun als Arrays übergeben

--- functions/html_templates/userdata.php
+++ functions/html_templates/userdata.php
@@ -142,6 +142,7 @@ function getAddressEditTemplate($data) {
 	// TEST WERTE FÜR DATA
 	$data['address'] = array(
 		array(
+			'addr_description' => 'Alte Adresse',
 			'street' => 'Ackerstraße',
 			'number' => '109',
 			'addr_extra' => '',
@@ -149,6 +150,7 @@ function getAddressEditTemplate($data) {
 			'city' => 'Düsseldorf'
 		),
 		array(
+			'addr_description' => 'Neue Adresse',
 			'street' => 'Gilbachstraße',
 			'number' => '9',
 			'addr_extra' => '',
@@ -163,7 +165,7 @@ function getAddressEditTemplate($data) {
 		<script src='".get_template_directory_uri()."/js/expandable_list.js'>
 		</script>
 		<script>
-		var tmp_address = \"<table class='form'><tr><td width='20%'>Straße / Nr.</td><td width='30%'><input type='text'name='street[]' placeholder='Straße' value='%%DATA-street%%'/></td><td width='30%'><input type='text' name='number[]'placeholder='Nr.' value='%%DATA-number%%'/></td><td width='20%'><input type='text' name='addr_extra[]' placeholder='(Zusatz)' value='%%DATA-addr_extra%%'/></td></tr><tr><td>Wohnort</td><td><input type='text' name='postal[]'placeholder='PLZ' value='%%DATA-postal%%'/></td><td><input type='text' name='city[]' placeholder='Stadt'value='%%DATA-city%%'/></td></tr></table>\";
+		var tmp_address = \"<table class='form'><tr><td>Beschreibung</td><td colspan='2'><input type='text' name='addr_description[]' placeholder='Privat/Geschäftlich/...' value='%%DATA-addr_description%%' /></td></tr><tr><td width='20%'>Straße / Nr.</td><td width='30%'><input type='text'name='street[]' placeholder='Straße' value='%%DATA-street%%'/></td><td width='30%'><input type='text' name='number[]'placeholder='Nr.' value='%%DATA-number%%'/></td><td width='20%'><input type='text' name='addr_extra[]' placeholder='(Zusatz)' value='%%DATA-addr_extra%%'/></td></tr><tr><td>Wohnort</td><td><input type='text' name='postal[]'placeholder='PLZ' value='%%DATA-postal%%'/></td><td><input type='text' name='city[]' placeholder='Stadt'value='%%DATA-city%%'/></td></tr></table>\";
 		setup_expandablecontent('expandablecontent-address', 'address', tmp_address, ".toJSArrayString($data['address']).", 1);
 		</script>";
 	return $resl;
