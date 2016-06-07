b0f1ce404d329a5525740843ed94eab6848fb19c
Alexander Schäfer <alexander.schaefer@uni-duesseldorf.de>
2016-06-07 09:27:39 +0200

    Dateien werden nun im Bewerbungsformular hochgeladen

--- functions/html_templates/userdata.php
+++ functions/html_templates/userdata.php
@@ -23,28 +23,6 @@ function extractData($data, $key) {
 	}
 }
 
-function createBirthdaySelector($withID) {
-	$resl = "<select id='day-$withID' name='birth_day[]' style='width: 3em; float: left; margin-right:10px'>";
-	for ($i=1; $i<=31; $i++) {
-		$resl .= "<option value='$i'>$i</option>";
-	}
-	$resl .= "</select>
-				<select id='month-$withID' name='birth_month[]' style='width: 5.5em; float: left;  margin-right:10px;'>";
-	$months = array('Januar','Februar','März','April','Mai','Juni','Juli','August','September','Oktober','November','Dezember');
-	for ($i=0; $i<12; $i++) {
-		$resl .= "<option value='$i'>".$months[$i]."</option>";
-	}
-	$resl .= "</select>
-				<select id='year-$withID' name='birth_year[]' style='width: 5em; float: left;  margin-right:10px;'>";
-	$min = date('Y')-18;
-	$max = $min-30;
-	for ($i=$min; $i>=$max; $i--) {
-		$resl .= "<option value='$i'>$i</option>";
-	}
-	$resl .= "</select>";
-	return $resl;
-}
-
 // Wandelt ein PHP Array mit nativen Datentypen in einen Strin um, der eine
 // JS Form eines Arrays mit den selben Daten repräsentiert
 function toJSArrayString($data) {
@@ -60,7 +38,7 @@ function getContactEditTemplate($data) {
 	$data['mail'] = array(
 		array(
 			'mail_description' => 'Privat',
-			'mail_content' => 'test@123.de'
+			'mail_content' => 'test@123.de',
 		),
 		array(
 			'mail_description' => 'Privat',
@@ -90,10 +68,10 @@ function getContactEditTemplate($data) {
 				Name
 			</td>
 			<td>	
-				<input type='text' name='first_name' placeholder='Vorname' value='".extractData($data, 'first_name')."'/>
+				<input type='text' name='first_name' placeholder='Vorname' value='".extractData($data['info'], 'first_name')."'/>
 			</td>
 			<td width='40%'>
-				<input type='text' name='nachname' placeholder='Nachname' value='".extractData($data, 'last_name')."'/>
+				<input type='text' name='nachname' placeholder='Nachname' value='".extractData($data['info'], 'last_name')."'/>
 			</td>
 		</tr>
 		<tr>
@@ -150,10 +128,10 @@ function getContactEditTemplate($data) {
 	<script src='".get_template_directory_uri()."/js/expandable_list.js'></script>
 	<script>
 	var tmp_mail = \"<td><input id='mail_description-%%FULL-ID%%' class='mail_content' type='text' name='mail_description[]' placeholder='Beschreibung' value='%%DATA-mail_description%%' style='width: 45%; margin-right: 10px;'/></td><td><input id='mail_content_%%FULL-ID%%' class='mail_content' type='email' name='email[]' placeholder='E-Mail' value='%%DATA-mail_content%%' style='width: 45%;'/></td>\";
-	setup_expandablecontent('expandablecontent-mail', 'mail', tmp_mail, ".toJSArrayString($data['mail']).", 1);
+	setup_expandablecontent('expandablecontent-mail', 'mail', tmp_mail, ".toJSArrayString($data['mails']).", 1);
 
 	var tmp_phone = \"<td><input id='phone_description-%%FULL-ID%%' class='phone_content' type='text' name='phone_description[]' placeholder='Beschreibung' value='%%DATA-phone_description%%' style='width: 45%; margin-right: 10px;'/></td><td><input id='phone_content_%%FULL-ID%%' class='phone_content' type='text' name='phone[]' placeholder='Telefonnummer' value='%%DATA-number%%' style='width: 45%;'/></td>\";
-	setup_expandablecontent('expandablecontent-phone', 'phone', tmp_phone, ".toJSArrayString($data['phone']).", 1);
+	setup_expandablecontent('expandablecontent-phone', 'phone', tmp_phone, ".toJSArrayString($data['phones']).", 1);
 	</script>";
 	return $resl;
 }
@@ -258,7 +236,7 @@ function getFileEditTemplate($data) {
 		</table>
 		<script src='".get_template_directory_uri()."/js/expandable_list.js'></script>
 		<script>
-		var tmp_files = \"<input id='%%desc_FULL-ID%%' type='text'name='filedescription[]' placeholder='Bezeichnung' value='%%DATA-filedescription%%' style='width: 45%;'/><input id='file_%%FULL-ID%%' type='file' style='width: 45%;'/>\";
+		var tmp_files = \"<input id='%%desc_FULL-ID%%' type='text'name='filedescription[]' placeholder='Bezeichnung' value='%%DATA-filedescription%%' style='width: 45%;'/><input id='file_%%FULL-ID%%' name='applicationfile[]' type='file' accept='*' style='width: 45%;'/>\";
 		setup_expandablecontent('expandablecontent-files', 'files', tmp_files, ".toJSArrayString($data['files']).", 1);
 		</script>";
 	return $resl;
