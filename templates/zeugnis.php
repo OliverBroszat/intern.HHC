fff95b491abf6d56714d1fae3957701b9cf2b4b9
OliverBroszat <Oliver.Broszat@hhc-duesseldorf.de>
2016-07-07 15:01:54 +0200

    Zweites Template wurde entfernt und der Aufbau der Datei wurde überarbeitet. Zusätzlich erhält das Mitglied nun eine Bestätigung oder Fehlermeldung beim anfordern des Zeugnisses. Außerdem werden die EIngabefelder beim ersten Aufruf der Seite nun vorgefüllt.

--- templates/zeugnis.php
+++ templates/zeugnis.php
@@ -5,10 +5,12 @@
 
 get_header();
 
-
+/**
+ * Wird aufgerufen, falls das Zeugnis bereits vom Mitglied ausgefüllt wurde
+ *und jetzt durch den Ressortleiter bewertet werden soll
+*/
 if($_GET){
     $zeungisID = substr($_GET['ID'], 3, strlen($_GET['ID']) - 6);
-    echo "zeug_:" . $zeungisID;
     $query = "SELECT c.id, prefix, first_name, last_name, birth_date, joined, m.left, r.name as ressort, aufgaben,
                 interneProjekte, workshops, externeProjekte, anmerkungen
 	        	from contact c
@@ -19,30 +21,107 @@ if($_GET){
 
 	$result = $wpdb->get_row($query);
 
+	$aufgaben = $result->aufgaben;
+	$interneProjekte = $result->interneProjekte;
+	$workshops = $result->workshops;
+	$externeProjekte = $result->externeProjekte;
+	$anmerkungen = $result->anmerkungen;
+
+	//Wird aufgerufen, wenn das Formular zum ersten Mal durch das Mitglied ausgefüllt wird
+	} else {
+	$usermail = wp_get_current_user() -> user_email;
+	 $query = "SELECT c.id, prefix, first_name, last_name, birth_date, joined, m.left, r.name as ressort
+	        	from contact c
+	        	  join member m on c.id = m.contact
+	        	  JOIN mail ma on ma.contact = c.id
+	        	  join ressort r on m.ressort = r.id
+	        	where ma.address = '$usermail'";
+	$result = $wpdb->get_row($query);
+	}
+
+	$vorname = $result->first_name;
+    $nachname = $result->last_name;
+	$geburtsdatum = $result->birth_date;
+	$anfangsdatum = $result->joined;
+	$enddatum = $result->left;
+	$ressort = strtoupper($result->ressort);
+
 	if($result->prefix == "Herr"){
 	    $geschlecht = "Männlich";
 	} else {
 	    $geschlecht = "Weiblich";
 	}
 
-    $vorname = $result->first_name;
-    $nachname = $result->last_name;
-	$geburtsdatum = $result->birth_date;
-	$anfangsdatum = $result->joined;
-	$enddatum = $result->left;
-	$ressort = $result->ressort;
-	$aufgaben = $result->aufgaben;
-	$interneProjekte = $result->interneProjekte;
-	$workshops = $result->workshops;
-	$externeProjekte = $result->externeProjekte;
-	$anmerkungen = $result->anmerkungen;
+    /**
+    *  Wird aufgerufen nachdem das Mitgleid sein Zeugnis angefpordert hat
+    *  dient der Speicherung der Eingegebenen Informationen in die Datenbank
+    *  der Darstellung der Information, ob die Anforderung des Zeugnisses
+    *  erfolgreich war und dem Versand der Email an den Ressortleiter
+     */
+
+	if($_POST){
+         get_currentuserinfo();
+
+        $useraddress = $current_user->user_email;
+
+        $query = "SELECT first_name, last_name
+                        from contact c join member m on c.id = m.contact
+                        join mail ma on ma.contact = c.id
+                        where ma.address = '$useraddress'";
+
+        $result = $wpdb->get_row($query);
+
+        $first_name = $result->first_name;
+        $last_name = $result->last_name;
+
+        $wpdb->insert(
+                'zeugnisse',
+                array(
+                        'contact' => $result->id,
+                        'aufgaben' => $_POST['aufgaben'],
+                        'interneProjekte' => $_POST['interneProjekte'],
+                        'workshops' => $_POST['workshops'],
+                        'externeProjekte' => $_POST['externeProjekte'],
+                        'anmerkungen' => $_POST['anmerkungen']
+                )
+        );
+
+        $query = "SELECT address
+                    from mail m
+                      join member mem on mem.contact = m.contact
+                      join ressort r on mem.ressort = r.id
+                    where m.description = 'HHC'
+                    AND position='Ressortleiter'
+                    AND r.id = (SELECT ressort from member where contact = '$userid')";
+
+        $result = $wpdb->get_row($query);
+
+        $zeugnis_url = get_template_directory()."/templates/zeugnis?ID=644$wpdb->insert_id". "823";
+
+        $to = $result->address;
+        $subject = "Zeugnisanforderung: $first_name $last_name";
+        $message = "Hallo ". substr($to, 0, strpos($to, '.')) . ",\n
+                          dein Ressormitglied $first_name $last_name hat ein Arbeitszeugnis angefordert.\n
+                          Bitte klicke auf den folgenden Link und fülle das Formular bezüglich der Mitarbeit
+                          des Mitglieds aus.\n\n
+                          $zeugnis_url";
+
+        if(mail($to, $subject, $message)){
+            echo "<div class='msg' style='background-color:green; font-size: 1em;'>
+                    Das Zeugnis wurde erfolgreich angefordert.
+                    </div>";
+        } else{
+            echo "<div class='msg' style='background-color:red; font-size: 1em;'>
+                    Es ist ein Fehler bei der Übertragung der Daten aufgetreten.
+                    </div>";
+        }
 	}
 ?>
 
 	<h1>Arbeitszeugnis</h1>
 
-	<form action="<?php echo "../zeugnisverarbeiten" ?>"method='POST' enctype='multipart/form-data'>	
-		
+	<form action="#" method='POST' enctype='multipart/form-data'>
+
 		<h2>Persönliche Angaben</h2>
 
 		<table class='form'>
@@ -61,7 +140,7 @@ if($_GET){
                       Geschlecht:
                       <select name='geschlecht'>
                         <option selected disabled hidden> <?php echo $geschlecht ?></option>
-                        <option value='Mänkkkkknlich'>Männlich</option>
+                        <option value='Männknlich'>Männlich</option>
                         <option value='Weiblich'>Weiblich</option>
                       </select>
                     </td>
@@ -77,7 +156,7 @@ if($_GET){
                   </tr>
                   <tr>
                     <td>
-                      Beginn der Mitgleidschaft:
+                      Beginn der Mitgliedschaft:
                       <input name="anfangsdatum" type="date" value="<?php echo $anfangsdatum?>">
                     </td>
                     <td>
