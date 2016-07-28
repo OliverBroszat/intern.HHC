19c1f45ec749d5b892a8deba2f8a52e80ecc9d94
Alexander Schäfer <alexander.schaefer@uni-duesseldorf.de>
2016-07-28 08:00:39 +0200

    ContactDataController angelegt

---
+++ OOP/model/DataController/ContactDataController/ContactDataController.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 19.07.16
+ * Time: 23:07
+ */
+
+/*
+ * Because we're outside the wordpress template directory, no wordpress functionality will be
+ * pre-includes for us :(
+ * In this block we include all necessary functions and an autoloader by loading the 'wp-load.php'.
+ * NOTE: on local host this file has a different path!
+ */
+if (!function_exists('serverIsRunningOnLocalHost')) {
+    function serverIsRunningOnLocalHost() {
+        $localHostAddresses = array('127.0.0.1', '::1');
+        $currentServerIPAddress = $_SERVER['REMOTE_ADDR'];
+        if(in_array($currentServerIPAddress, $localHostAddresses)){
+            return true;
+        }
+        return false;
+    }
+}
+
+if (!function_exists('loadWordpressFunctions')) {
+    function loadWordpressFunctions() {
+        $serverRootPath = realpath($_SERVER["DOCUMENT_ROOT"]);
+        if (serverIsRunningOnLocalHost()) {
+            $serverRootPath = realpath($_SERVER["CONTEXT_DOCUMENT_ROOT"]).'/wordpress';
+        }
+        require_once("$serverRootPath/wp-load.php");
+    }
+}
+
+loadWordpressFunctions();
+
+
+/**
+ * Class BaseDataController
+ *
+ * Provides OO access to wordpress database and exception handling
+ */
+class ContactDataController {
+
+}
