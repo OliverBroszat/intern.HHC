666276abeda33bf577d9f9a6e58bc800bc99ea87
Alexander Schäfer <alexander.schaefer@uni-duesseldorf.de>
2016-08-07 15:23:43 +0200

    Added method updateDataWithContactId($id) to ContactProfile
    Given any contact id, the object will update all foreign keys in the address, mail, phone and study data.

--- OOP/model/DataController/ContactDataController/ContactProfile.php
+++ OOP/model/DataController/ContactDataController/ContactProfile.php
@@ -16,6 +16,22 @@ class ContactProfile {
 		$this->studyDatabaseRows = $studyDatabaseRows;
 	}
 
+	public function updateDataWithContactId($id) {
+		$this->contactDatabaseRow->setValueForKey('id', $id);
+		$this->updateContactIdFor($this->addressDatabaseRows);
+		$this->updateContactIdFor($this->mailDatabaseRows);
+		$this->updateContactIdFor($this->phoneDatabaseRows);
+		$this->updateContactIdFor($this->studyDatabaseRows);
+	}
+
+	private function updateContactIdFor($rows) {
+		// We assume that all contact related tables store the foreign key to the actual contact in a column named 'contact'
+		$id = $this->contactDatabaseRow->getValueForKey('id');
+		foreach ($rows as $row) {
+			$row->setValueForKey('contact', $id);
+		}
+	}
+
 }
 
 ?>
