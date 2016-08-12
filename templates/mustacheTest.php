0de667b76e4d033b222f6eea4dcbea850dc929fb
Alexander Schäfer <alexander.schaefer@uni-duesseldorf.de>
2016-08-13 01:50:57 +0200

    Added Mustache JS Framework
    Check out mustache test template
    NOTE: path to mustache.js has to be edited!!!

--- templates/mustacheTest.php
+++ templates/mustacheTest.php
@@ -14,12 +14,14 @@ function new_paragraph($headline) {
 }
 
 get_header();
-
 ?>
+
+<script type='text/javascript' src='http://localhost/~alex/wordpress/wp-content/themes/intern-hhc/import/js/Mustache/mustache_v2.2.1.js'></script>
 
 <div class="outer">
 	<h1 style="text-transform: none;">intern.HHC</h1>
 	<div class="panel">
+	<div id='text'></div>
 <?php
 
 $options =  array('extension' => '.html');
@@ -33,6 +35,18 @@ echo $m->render('test', array('name' => 'Peter'));
 	</div>
 </div>
 
+<script>
+var view = {
+  title: "Joe",
+  calc: function () {
+    return 2 + 4;
+  }
+};
+
+var output = Mustache.render("{{title}} spends {{calc}}", view);
+console.log(output);
+document.getElementById('text').innerHTML = output;
+</script>
 
 
 
