f38cb9498036d3ffc64a24ca4934adc4dca5da22
Alexander Schäfer <alexander.schaefer@uni-duesseldorf.de>
2016-08-14 07:17:45 +0200

    Created ExpandableList V2
    ExpandableList now completely works with mustache!
    NOTE: current templates don't work

--- templates/mustacheTest.php
+++ templates/mustacheTest.php
@@ -14,47 +14,31 @@ function new_paragraph($headline) {
 }
 
 get_header();
+$data = array(
+	'personData' => array(
+		array('description' => 'Privat', 'mail' => 'a@b.de'),
+		array('description' => 'Privat2', 'mail' => 'a1@b.de'),
+		array('description' => 'Privat3', 'mail' => 'a2@b.de')
+	)
+);
 ?>
 
 <script type='text/javascript' src='http://localhost/~alex/wordpress/wp-content/themes/intern-hhc/import/js/Mustache/mustache_v2.2.1.js'></script>
+<script type='text/javascript' src='http://localhost/~alex/wordpress/wp-content/themes/intern-hhc/js/expandableList.js'></script>
 
 <div class="outer">
 	<h1 style="text-transform: none;">intern.HHC</h1>
 	<div class="panel">
 	<div id='text'></div>
-<?php
 
-$options =  array('extension' => '.html');
-$root = $root = get_template_directory();
-$m = new Mustache_Engine(array(
-    'loader' => new Mustache_Loader_FilesystemLoader($root . '/views', $options),
-));
-echo $m->render('test', array('name' => 'Peter'));
-?>
+	<exl-container id='mails' template='name-list' source='personData' class='exl-container'></exl-container><br>
 
+	<script>
+		initializeExpandableList();
+		setupExlContainerWithID('mails', <?php echo json_encode($data); ?>);
+	</script>
 	</div>
 </div>
-
-<script>
-$.get('template.mst', function(template) {
-	var rendered = Mustache.render(template, {name: "Luke"});
-	$('#target').html(rendered);
-});
-
-var view = {
-  title: "Joe",
-  calc: function () {
-    return 2 + 4;
-  }
-};
-
-var output = Mustache.render("{{title}} spends {{calc}}", view);
-console.log(output);
-document.getElementById('text').innerHTML = output;
-</script>
-
-
-
 
 
 
