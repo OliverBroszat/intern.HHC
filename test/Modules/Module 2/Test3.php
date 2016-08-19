27e180a0d92f318ab4e4017baef1d613e51d2272
Alexander Schäfer <alexander.schaefer@uni-duesseldorf.de>
2016-08-19 13:53:47 +0200

    Add dummy test cases

---
+++ test/Modules/Module 2/Test3.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * Because we're outside the wordpress template directory, no wordpress functionality will be
+ * pre-includes for us :(
+ * In this block we include all necessary functions and an autoloader by loading the 'wp-load.php'.
+ * NOTE: on local host this file has a different path!
+ */
+if (!function_exists('serverIsRunningOnLocalHost')) {
+    function serverIsRunningOnLocalHost() {
+        $localHostAddresses = array('127.0.0.1', '::1');
+        $currentServerIPAddress = $_SERVER['REMOTE_ADDR'];
+        if(in_array($currentServerIPAddress, $localHostAddresses)){
+            return true;
+        }
+        return false;
+    }
+}
+
+if (!function_exists('loadWordpressFunctions')) {
+    function loadWordpressFunctions() {
+        $serverRootPath = realpath($_SERVER["DOCUMENT_ROOT"]);
+        if (serverIsRunningOnLocalHost()) {
+            $serverRootPath = realpath($_SERVER["CONTEXT_DOCUMENT_ROOT"]).'/wordpress';
+        }
+        require_once("$serverRootPath/wp-load.php");
+    }
+}
+
+loadWordpressFunctions();
+
+// I can be sure, that I am in a well suited environment
+// All modules are imported, I got all wp functions and
+// the auto loader is set up
+// phpUnit is also loaded!
+// Everything is just fine :) Now test!
+
+class Test1 extends TestCase {
+
+	protected $information = "This is my third test case";
+
+}
+
+?>
