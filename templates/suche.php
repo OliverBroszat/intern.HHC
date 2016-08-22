ee9d606f0f6e4daeaaeb506ca3aa67f4b0d9d05e
Alexander Schäfer <alexander.schaefer@uni-duesseldorf.de>
2016-08-23 00:20:16 +0200

    Applied Mustache to Filterbox

--- templates/suche.php
+++ templates/suche.php
@@ -99,35 +99,83 @@ $sort_categories = array(
 
 // Filter-Data
 global $wpdb;
-$ressorts = res_to_array($wpdb->get_results("SELECT name FROM Ressort"));
-$positions = res_to_array($wpdb->get_results("SELECT position FROM Member"));
-$status = array('0', '1');
-$schools = res_to_array($wpdb->get_results("SELECT school FROM Study"));
+$filter_values = array(
+	res_to_array($wpdb->get_results("SELECT name FROM Ressort")),
+	res_to_array($wpdb->get_results("SELECT position FROM Member")),
+	array('0', '1'),
+	res_to_array($wpdb->get_results("SELECT school FROM Study"))
+);
+$filterTitles = array(
+	'Ressort', 'HHC Position', 'HHC Status', 'Universität'
+);
+$filter_list_names = array(
+	'ressort', 'position', 'status', 'uni'
+);
+$numberOfColumns = array(2, 2, 2, 1);
 
+$filters = array();
+for ($filterNumber=0; $filterNumber < sizeof($filterTitles); $filterNumber++) {
+	$currentFilterData = array();
+	$currentFilterData['title'] = $filterTitles[$filterNumber];
+	$currentFilterData['width'] = 100 / $numberOfColumns[$filterNumber];
+	$currentFilterData['list_name'] = $filter_list_names[$filterNumber];
+	$currentFilterData['num_of_columns'] = $numberOfColumns[$filterNumber];
 
+	$currentFilterData['filter_rows'] = array();
+	$sizeOfRows = $numberOfColumns[$filterNumber];
+	$numberOfFiltersInCurrentRow = 0;
+	$currentFilterValues = $filter_values[$filterNumber];
+	var_dump($currentFilterValues);
+	echo '<br>';
+	$currentRow = array('row_items' => array());
+	foreach ($currentFilterValues as $value) {
+		$currentItem = array(
+			'item_value' => $value,
+			'item_display_name' => uppercase(bool_to_lbl($value))
+		);
+		if ($numberOfFiltersInCurrentRow >= $sizeOfRows) {
+			array_push($currentFilterData['filter_rows'], $currentRow);
+			$currentRow = array('row_items' => array());
+			$numberOfFiltersInCurrentRow = 0;
+		}
+		array_push($currentRow['row_items'], $currentItem);
+		$numberOfFiltersInCurrentRow++;
+	}
+	if (sizeof($currentRow) > 0) {
+		array_push($currentFilterData['filter_rows'], $currentRow);
+	}
+	array_push($filters, $currentFilterData);
+}
+
+// $filters = array(
+// 	array(
+// 		'title' => 'Filter 1',
+// 		'cols' => 2,
+// 		'col_width' => 50,
+// 		'filter_name' => 'inID',
+// 		'filter_items' => array(
+// 			array(
+// 				'rows' => array(
+// 					array('data' => array(
+// 						array('value' => 'value1', 'display_name' => 'NAME1'),
+// 						array('value' => 'value2', 'display_name' => 'NAME2')
+// 					)),
+// 					array('data' => array(
+// 						array('value' => 'value3', 'display_name' => 'NAME3'),
+// 						array('value' => 'value4', 'display_name' => 'NAME4')
+// 					)),
+// 				)
+// 			)
+// 		)
+// 	),
+// 	array('title' => 'Filter 2', 'filter_items' => array()),
+// 	array('title' => 'Filter 3', 'filter_items' => array())
+// );
 
 $data = array(
 	'root_path' => $root_uri,
 	'sort_categories' => $sort_categories,
-	'filter_categories' => array(
-		array('title' => 'Filter 1', 'filter_items' => array(
-			array(
-				'width' => 50,
-				'rows' => array(
-					array('data' => array(
-						array('name' => 'name1', 'value' => 'value1', 'display_name' => 'NAME1'),
-						array('name' => 'name2', 'value' => 'value2', 'display_name' => 'NAME2')
-					)),
-					array('data' => array(
-						array('name' => 'name3', 'value' => 'value3', 'display_name' => 'NAME3'),
-						array('name' => 'name4', 'value' => 'value4', 'display_name' => 'NAME4')
-					)),
-				)
-			)
-		)),
-		array('title' => 'Filter 2', 'filter_items' => array()),
-		array('title' => 'Filter 3', 'filter_items' => array())
-	)
+	'filters' => $filters
 );
 
 echo $mustache->render('member_search', $data);
