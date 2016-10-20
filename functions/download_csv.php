0552b03e6f7c0aebe448cf70fbcb634bcb7537fd
Daniel Högel <hoegel.daniel@gmail.com>
2016-10-20 21:24:21 +0200

    Timestamp

--- functions/download_csv.php
+++ functions/download_csv.php
@@ -9,9 +9,10 @@ if(in_array($_SERVER['REMOTE_ADDR'], $localhost)){
 require_once("$root/wp-load.php");
 
 
+date_default_timezone_set('Europe/Berlin');
 // output headers so that the file is downloaded rather than displayed
 header('Content-Type: text/csv; charset=utf-8');
-header('Content-Disposition: attachment; filename=mitgliederliste.csv');
+header('Content-Disposition: attachment; filename=mitgliederliste_'.date('Y-m-d_H-m-s').'.csv');
 
 // create a file pointer connected to the output stream
 $output = fopen('php://output', 'w');
