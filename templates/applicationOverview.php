b1023d04c2b0ba63220616ca97753ab85dbf15f1
Daniel Högel <hoegel.daniel@gmail.com>
2016-10-31 00:12:38 +0100

    ApplicationOverview Template works
    Sort and Filter options and action buttons are only placeholders

--- templates/applicationOverview.php
+++ templates/applicationOverview.php
@@ -25,14 +25,35 @@ foreach ($applications as $application) {
 $applicationProfileController = new ApplicationProfileController();
 $applicationProfiles = $applicationProfileController->createMultipleApplicationProfilesByIDs($applicationIDs);
 
-
-// DEBUG Output
-arr_to_list($applicationProfiles);
-exit();
-
+// transform ApplicationProfiles to data (array) usable with mustache
 $translator = new Translator();
+$applications = $translator->transformMultipleApplicationProfilesToData($applicationProfiles);
 
-$data = array('test' => 'Hallo Welt!');
+$numberOfApplications = sizeof($applications);
+
+$sort_categories = array(
+	array('value' => 'Contact.last_name', 'name' => 'Nachname'),
+	array('value' => 'Contact.first_name', 'name' => 'Vorname'),
+	array('value' => 'Application.state', 'name' => 'State'),
+	array('value' => 'Application.income', 'name' => 'Income'),
+);
+
+
+// Filter-Data
+$filters = new FilterLists();
+$filtersHTML = array();
+foreach ($filters->getApplicationFilter() as $filter) {
+	array_push($filtersHTML, $filter->createHtmlTable());
+}
+
+$data = array(
+	'applications' => $applications,
+	'numberOfApplications' => $numberOfApplications,
+	'root_path' => $root_uri,
+	'sort_categories' => $sort_categories,
+	'filters' => $filtersHTML,
+
+);
 
 // RENDER applicationOverview Template with Mustache
 $html = $mustache->render('applicationOverview', $data);
