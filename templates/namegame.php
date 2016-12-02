cc4b2fac76b33d50907632f64bdcca8e45c416f3
Daniel Högel <hoegel.daniel@gmail.com>
2016-12-03 00:40:01 +0100

    Full resolution images

--- templates/namegame.php
+++ templates/namegame.php
@@ -22,7 +22,7 @@
    public function __construct($contactRow) {
      $this->firstName = $contactRow->getValueForKey('first_name');
      $this->lastName = $contactRow->getValueForKey('last_name');
-     $this->imageUrl = wp_get_attachment_image_src($contactRow->getValueForKey('image'))[0];
+     $this->imageUrl = wp_get_attachment_image_src($contactRow->getValueForKey('image'), $size='')[0];
      $this->ressortName = $contactRow->getValueForKey('ressortName');
      $this->ressortDescription = $contactRow->getValueForKey('ressortDescription');
      $this->id = uniqid('', true);
