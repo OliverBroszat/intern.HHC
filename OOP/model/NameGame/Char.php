98eae31a38f9a0a3815b894a7427ead29d6ddc4d
Daniel Högel <hoegel.daniel@gmail.com>
2016-12-09 09:58:50 +0100

    Encode Image
    Beim Versuch, den mime type zu ermitteln, bekomme ich einen Fehler: Warning: finfo_file(): Failed identify data 0:(null)
    Aber es funktioniert scheinbar auch ohne.

--- OOP/model/NameGame/Char.php
+++ OOP/model/NameGame/Char.php
@@ -8,6 +8,8 @@ class Char {
  public $firstName;
  public $lastName;
  public $imageUrl;
+ public $imageData;
+ public $imageMime;
  public $ressortName;
  public $ressortDescription;
  public $id;
@@ -16,8 +18,21 @@ class Char {
    $this->firstName = $contactRow->getValueForKey('first_name');
    $this->lastName = $contactRow->getValueForKey('last_name');
    $this->imageUrl = wp_get_attachment_image_src($contactRow->getValueForKey('image'), $size='')[0];
+   $this->imageData = $this->getImageDataByUrl($this->imageUrl);
+   // $this->imageMime = $this->getImageMimeByUrl($this->imageUrl);
    $this->ressortName = $contactRow->getValueForKey('ressortName');
    $this->ressortDescription = $contactRow->getValueForKey('ressortDescription');
    $this->id = uniqid('', true);
+ }
+
+ private function getImageDataByUrl($url){
+ 	return (base64_encode(file_get_contents($url)));
+ }
+
+ private function getImageMimeByUrl($url) {
+ 	$finfo = finfo_open(FILEINFO_MIME_TYPE); // return mime type ala mimetype extension
+	$mime =  finfo_file($finfo, $url);
+ 	finfo_close($finfo);
+ 	return $mime;
  }
 }
--- views/namegame/image.php
+++ views/namegame/image.php
@@ -1,5 +1,5 @@
 <?php global $sessionManager ?>
 
 <div class="img-container ui segment">
-  <img class="ui segment" src="<?=$sessionManager->getSelectedChar()->imageUrl?>" >
+  <img class="ui segment" src="data:;base64,<?= $sessionManager->getSelectedChar()->imageData ?>"
 </div>
